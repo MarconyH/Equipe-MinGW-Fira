8472c65ccd713dfafa093e59cd8a001665396289
Leonel <franciscoleonelfds@gmail.com>
2024-10-21 17:15:23 -0300

    BOMBA - L

--- src/main_funciona.cpp
+++ src/main_funciona.cpp
@@ -8,21 +8,21 @@
 #define TRIGC A2 // Pino Trig Sensor Centro
 #define ECHOC A3 // Pino Echo Sensor Centro
 
-#define TRIGD A4  // Pino Trig Sensor Direita
+#define TRIGD A4 // Pino Trig Sensor Direita
 #define ECHOD A5 // Pino Echo Sensor Direita
 
-#define PINO_CH1 6
-#define PINO_CH2 2  
-#define PINO_CH3 4
-#define PINO_CH4 5
+#define PINO_CH1 3
+#define PINO_CH2 2
+#define PINO_CH3 10
+#define PINO_CH4 11
 
-#define ENA 5 // ENA PWM Motor Esquerdo
-#define ENB 6 // ENB PWM Motor Direito
+#define ENA 6 // ENA PWM Motor Esquerdo
+#define ENB 5 // ENB PWM Motor Direito
 
-#define IN1 2 // DIR Motor Esquerdo
-#define IN2 4 // DIR Motor Esquerdo
+#define IN1 7 // DIR Motor Esquerdo
+#define IN2 7 // DIR Motor Esquerdo
 
-#define IN3 7 // DIR Motor Direito
+#define IN3 8 // DIR Motor Direito
 #define IN4 8 // DIR Motor Direito
 
 Ultrasonic sensorD(TRIGD, ECHOD);
@@ -34,11 +34,7 @@ float distanciaE;
 double distanciaC;
 float distanciaD;
 
-int estado;
-int ultimo_estado;
-boolean sentido;
-
-//Velocidade em rpm da roda esquerda e direita
+// Velocidade em rpm da roda esquerda e direita
 int velocidadeD = 0;
 int velocidadeE = 0;
 
@@ -46,19 +42,41 @@ float speed = 1.0; // throttle in % percent
 unsigned long time;
 /*Parâmetros para ajustar*/
 
-//Declaracao das variaveis auxiliares para o calculo da velocidade
-unsigned long contador1;
-unsigned long contador2;
-unsigned long contador3;
-unsigned long contador4;
+// Robocore - fonte
+// Declaracao das variaveis auxiliares para a verificacao do sentido
+int estado_sensor1;
+int ultimo_estado_sensor1;
+boolean sentido_sensor1;
+
+int estado_sensor2;
+int ultimo_estado_sensor2;
+boolean sentido_sensor2;
+
+// Declaracao das variaveis auxiliares para o calculo da velocidade
+unsigned long contador1_sensor1;
+unsigned long contador2_sensor1;
+
+unsigned long contador1_sensor2;
+unsigned long contador2_sensor2;
+
 const int NUMERO_CONTADORES = 2;
 const int NUMERO_LEITURAS = 2;
-//Variavel de numero de dentes do disco de leitura
-const int NUMERO_DENTES = 10; //Altere se necessario
+// Variavel de numero de dentes do disco de leitura
+const int NUMERO_DENTES = 6; // Altere se necessario
 
-//Declaracao das variaveis auxiliares para a temporizacao de um minuto
-unsigned long tempo_antes_direita = 0;
-unsigned long tempo_antes_esquerda = 0;
+// Declaracao das variaveis auxiliares para a temporizacao de um minuto
+unsigned long tempo_antes_sensor1 = 0;
+unsigned long tempo_antes_sensor2 = 0;
+const long MINUTO = 1000;
+// Robocore - fonte
+
+// Declaracao das variaveis auxiliares para a temporizacao de um minuto
+unsigned long tempo_antes_direita_sensor1 = 0;
+unsigned long tempo_antes_esquerda_sensor1 = 0;
+
+unsigned long tempo_antes_direita_sensor2 = 0;
+unsigned long tempo_antes_esquerda_sensor2 = 0;
+
 const long tempo_atual = 1000;
 
 //-----------------------------------------------
@@ -70,7 +88,7 @@ const long tempo_atual = 1000;
 
 float MAX_DELTA = 40;
 
-float MAX_VOLTAGE = 113;
+float MAX_VOLTAGE = 92;
 float MIN_VOLTAGE_ESQ = 50;
 float MIN_VOLTAGE_DIR = 30;
 
@@ -83,8 +101,6 @@ double SetpointCentral = 15;
 double kpCentral = 5.0, kiCentral = 3.5, kdCentral = 2.0;
 PID PIDc(&distanciaC, &OutputC, &SetpointCentral, kpCentral, kiCentral,
          kdCentral, REVERSE);
-
-
 
 void ler_sensores()
 {
@@ -110,66 +126,72 @@ void ler_sensores()
           sensorC.convert(
               microsec, Ultrasonic::CM)); // filtro(sensorC.convert(microsec,
                                           // Ultrasonic::CM), distanciaC, 1.0);
-  contador_direita();
-  contador_esquerda();
 }
 
-void contador_direita(){
-  if ((millis() - tempo_antes_direita) > tempo_atual) { //A cada um segundo
+// Robocore - Fonte
 
-  //Calcula a velocidade e exibe no monitor
-  int media = (contador1 + contador2) / (NUMERO_CONTADORES); //Calcula a media dos contadores
-  velocidadeD = (media / (NUMERO_DENTES  * NUMERO_LEITURAS))*60; //Calcula a velocidade de acordo com o numero de dentes do disco
+// Funcao de interrupcao
+void contador_pulso2_sensor1()
+{
 
-  //Zera os contadores e reinicia a contagem de tempo.
-  contador1 = 0;
-  contador2 = 0;
-  tempo_antes_direita = millis();
+  // Incrementa o contador
+  contador2_sensor1++;
+
+  // Verifica o sentido de rotacao do motor
+  estado_sensor1 = digitalRead(PINO_CH2);
+  if (ultimo_estado_sensor1 == LOW && estado_sensor1 == HIGH)
+  {
+    if (digitalRead(PINO_CH1) == LOW)
+    {
+      sentido_sensor1 = true;
+    }
+    else
+    {
+      sentido_sensor1 = false;
+    }
   }
+  ultimo_estado_sensor1 = estado_sensor1;
 }
 
-void contador_esquerda(){
-  if ((millis() - tempo_antes_esquerda) > tempo_atual) { //A cada um segundo
+// Funcao de interrupcao
+void contador_pulso1_sensor1()
+{
 
-  //Calcula a velocidade
-  int media = (contador3 + contador4) / (NUMERO_CONTADORES); //Calcula a media dos contadores
-  velocidadeE = (media / (NUMERO_DENTES  * NUMERO_LEITURAS))*60; //Calcula a velocidade de acordo com o numero de dentes do disco
+  // Incrementa o contador
+  contador1_sensor1++;
+}
 
-  //Zera os contadores e reinicia a contagem de tempo.
-  contador3 = 0;
-  contador4 = 0;
-  tempo_antes_esquerda = millis();
+void contador_pulso2_sensor2()
+{
+
+  // Incrementa o contador
+  contador2_sensor2++;
+
+  // Verifica o sentido de rotacao do motor
+  estado_sensor1 = digitalRead(PINO_CH4);
+  if (ultimo_estado_sensor2 == LOW && estado_sensor2 == HIGH)
+  {
+    if (digitalRead(PINO_CH3) == LOW)
+    {
+      sentido_sensor2 = true;
+    }
+    else
+    {
+      sentido_sensor2 = false;
+    }
   }
-
+  ultimo_estado_sensor2 = estado_sensor2;
 }
 
-void contador_pulso4() {
+// Funcao de interrupcao
+void contador_pulso1_sensor2()
+{
 
-  //Incrementa o contador
-  contador4++;
+  // Incrementa o contador
+  contador1_sensor2++;
 }
 
-
-void contador_pulso3() {
-
-  //Incrementa o contador
-  contador3++;
-}
-
-//Funcao de interrupcao
-void contador_pulso2() {
-
-  //Incrementa o contador
-  contador2++;
-}
-
-//Funcao de interrupcao
-void contador_pulso1() {
-
-  //Incrementa o contador
-  contador1++;
-}
-
+// Robocore - Fonte
 
 void imprimeDistancias()
 {
@@ -200,12 +222,8 @@ void acelera(float vel_esquerda, float vel_direita)
   int vel_direita_int = ceil(tratamento((vel_direita)));
   int vel_esquerda_int = ceil(tratamento((vel_esquerda)));
 
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
   analogWrite(ENB, vel_esquerda_int);
 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
   analogWrite(ENA, vel_direita_int);
 }
 
@@ -238,10 +256,10 @@ void setup()
   pinMode(PINO_CH2, INPUT);
   pinMode(PINO_CH1, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(PINO_CH1), contador_pulso1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PINO_CH2), contador_pulso2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PINO_CH3), contador_pulso3, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PINO_CH4), contador_pulso4, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PINO_CH1), contador_pulso1_sensor1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PINO_CH2), contador_pulso2_sensor1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PINO_CH3), contador_pulso1_sensor2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PINO_CH4), contador_pulso2_sensor2, CHANGE);
 
   PIDc.SetSampleTime(10);
   PIDc.SetMode(AUTOMATIC);
@@ -254,9 +272,7 @@ void setup()
   // {
   //   PIDc.SetOutputLimits(MIN_VOLTAGE_ESQ, MAX_VOLTAGE);
   // }
-    PIDc.SetOutputLimits(70, MAX_VOLTAGE);
-
-    
+  PIDc.SetOutputLimits(70, MAX_VOLTAGE);
 
   ler_sensores();
   delay(2000);
@@ -399,65 +415,139 @@ void throttle()
   }
 }
 
-//posição 0 esquerda, 1 centro, 2 direita
+// posição 0 esquerda, 1 centro, 2 direita
 int *livre()
 {
   int vector[3] = {1, 0, 1};
-  //se o sensor da frente for maior que 30 centímetros, então está livre
-  //sensorC >= 30 ? vector[1] = 1: vector[1] = 0;
+  // se o sensor da frente for maior que 30 centímetros, então está livre
+  // sensorC >= 30 ? vector[1] = 1: vector[1] = 0;
 
   return vector;
 }
 
+void rotacao_RPM_sensor1()
+{
+  // Verifica a contagem de tempo e exibe as informacoes coletadas do motor
+  // if ((millis() - tempo_antes) > MINUTO)
+  // { // A cada minuto
+
+  // Verifica a variavel "sentido"
+  //  if (sentido) { //Se ela for verdadeira ("true")
+  //    Serial.print("Sentido: Horario");
+  //    Serial.print("       |  ");
+  //  } else { //Se ela for falsa ("false")
+  //    Serial.print("Sentido: Anti-Horario");
+  //    Serial.print("  |  ");
+  //  }
+
+  // Calcula a velocidade e exibe no monitor
+  int media = (contador1_sensor1 + contador2_sensor1) / (NUMERO_CONTADORES); // Calcula a media dos contadores
+  int velocidade = media / (NUMERO_DENTES * NUMERO_LEITURAS);                // Calcula a velocidade de acordo com o numero de dentes do disco
+  Serial.print("Velocidade: ");
+  Serial.print(velocidade);
+  Serial.println(" RPM");
+
+  // Zera os contadores e reinicia a contagem de tempo.
+  contador1_sensor1 = 0;
+  contador2_sensor1 = 0;
+  // tempo_antes = millis();
+  // }
+}
+
+void rotacao_RPM_sensor2()
+{
+  // Verifica a contagem de tempo e exibe as informacoes coletadas do motor
+  // if ((millis() - tempo_antes) > MINUTO)
+  // { // A cada minuto
+
+  // Verifica a variavel "sentido"
+  //  if (sentido) { //Se ela for verdadeira ("true")
+  //    Serial.print("Sentido: Horario");
+  //    Serial.print("       |  ");
+  //  } else { //Se ela for falsa ("false")
+  //    Serial.print("Sentido: Anti-Horario");
+  //    Serial.print("  |  ");
+  //  }
+
+  // Calcula a velocidade e exibe no monitor
+  int media = (contador1_sensor2 + contador2_sensor2) / (NUMERO_CONTADORES); // Calcula a media dos contadores
+  int velocidade = media / (NUMERO_DENTES * NUMERO_LEITURAS);                // Calcula a velocidade de acordo com o numero de dentes do disco
+  Serial.print("Velocidade: ");
+  Serial.print(velocidade);
+  Serial.println(" RPM");
+
+  // Zera os contadores e reinicia a contagem de tempo.
+  contador1_sensor2 = 0;
+  contador2_sensor2 = 0;
+  // tempo_antes = millis();
+  // }
+}
+
+
 void loop()
 {
+  // ler_sensores();
+  // float delta = distanciaE - distanciaD;
+  // int delta_velocidade = velocidadeE - velocidadeD;
+
+  // acelera(100, 100 + delta_velocidade);
+
+  // int *vector = livre();
+  // if(vector[2])
+  // {
+  //   // virar_direita();
+  // }
+  // if(vector[1])
+  // {
+  //   // frente();
+  //   if (!vector[2] && !vector[0])
+  //   {
+  //     ajuste(delta);
+  //   }
+
+  // }
+  // else
+  // {
+  //   if(vector[0])
+  //   {
+  //     // virar_esquerda();
+  //   }
+  //   else
+  //   {
+  //     // virar_direita();
+  //   }
+  // }
   while (true)
   {
-    
+
     for (int i = 0; i <= 100; i += 10)
     {
-      int tempo_atual = millis();
-      acelera(i,i);
-      while (tempo_atual - millis() < 1000 * 60 * 5)
+      unsigned long tempo_atual = millis();
+
+      acelera(70, 70);
+
+      while (millis() - tempo_atual < 1000 * 10 * 1)
       {
+        /* code */
+
+        Serial.print("A diferença entre o millis e tempo atual: ");
+        Serial.print(millis() - tempo_atual);
+        Serial.println();
+
         ler_sensores();
         Serial.print("i: ");
         Serial.println(i);
         Serial.print("Velocidade Esquerda: ");
-        Serial.println(velocidadeE);
+        rotacao_RPM_sensor2();
         Serial.print("Velocidade Direita: ");
-        Serial.println(velocidadeD);
+        rotacao_RPM_sensor1();
       }
-      delay(1000); 
+      //  delay(1000);
     }
-    while (true)
-    {
-      acelera(0,0);
-    }
-    
-    
-    //int diferenca = velocidadeE - velocidadeD;
-    //seguir_em_frente(velocidadeE, velocidadeD + diferenca);
-    /*
-    int *vector = livre();
-    if(vector[2])
-    {
-      virar_direita();
-    }
-    if(vector[1])
-    {
-      frente();
-    }
-    else
-    {
-      if(vector[0])
-      {
-        virar_esquerda();
-      }
-      else
-      {
-        virar_direita();
-      }
-    }*/
+
+    // while (true)
+    // {
+    //   acelera(0, 0);
+    // }
   }
 }
